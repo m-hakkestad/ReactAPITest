08c6913047bd31c2f824c0afa77a237564903257
m-hakkestad <m.hakkestad@gmail.com>
2019-09-19 13:56:31 +0200

    Change to right name in package.json

--- src/server.js
+++ src/server.js
@@ -125,12 +125,12 @@ app.use('/graphql', expressGraphQL({
 }));
 
 if(process.env.NODE_ENV === 'production') { 
-  app.use(express.static(path.join(__dirname, 'client/build')));  
+  app.use(express.static(path.join(__dirname, 'testgui/build')));  
     app.get('*', (req, res) => {    
-      res.sendfile(path.join(__dirname = 'client/build/index.html'));  })}
+      res.sendfile(path.join(__dirname = 'testgui/build/index.html'));  })}
 
 app.get('*', (req, res) => {    
-  res.sendfile(path.join(__dirname = 'client/build/index.html'));  });
+  res.sendfile(path.join(__dirname = 'testgui/build/index.html'));  });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT,() => console.log(`🚀 Server ready at ${PORT}`))
