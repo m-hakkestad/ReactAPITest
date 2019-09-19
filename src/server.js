497b392aea9b1cb991d2647086ddcade28881910
m-hakkestad <m.hakkestad@gmail.com>
2019-09-19 14:17:08 +0200

    removed testgui from buildpath

--- src/server.js
+++ src/server.js
@@ -125,12 +125,12 @@ app.use('/graphql', expressGraphQL({
 }));
 
 if(process.env.NODE_ENV === 'production') { 
-  app.use(express.static(path.join(__dirname, 'testgui/build')));  
+  app.use(express.static(path.join(__dirname, '/build')));  
     app.get('*', (req, res) => {    
-      res.sendfile(path.join(__dirname = 'testgui/build/index.html'));  })}
+      res.sendfile(path.join(__dirname = '/build/index.html'));  })}
 
 app.get('*', (req, res) => {    
-  res.sendfile(path.join(__dirname = 'testgui/build/index.html'));  });
+  res.sendfile(path.join(__dirname = '/build/index.html'));  });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT,() => console.log(`🚀 Server ready at ${PORT}`))
