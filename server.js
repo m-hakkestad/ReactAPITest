ef79f2cca1d10851aad93e6ccc7e7230125212ea
m-hakkestad <m.hakkestad@gmail.com>
2019-09-20 09:41:04 +0200

    New structure and Heroku support

--- server.js
+++ server.js
@@ -125,12 +125,11 @@ app.use('/graphql', expressGraphQL({
 }));
 
 if(process.env.NODE_ENV === 'production') { 
-  app.use(express.static(path.join(__dirname, '/build')));  
-    app.get('*', (req, res) => {    
-      res.sendfile(path.join(__dirname = '/build/index.html'));  })}
-
-app.get('*', (req, res) => {    
-  res.sendfile(path.join(__dirname = '/build/index.html'));  });
+  app.use(express.static(path.join(__dirname, 'client/build')));  
+  app.get('*', (req, res) => {    
+    res.sendFile(path.resolve(__dirname,'client/build/index.html'));  
+  })
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT,() => console.log(`🚀 Server ready at ${PORT}`))
